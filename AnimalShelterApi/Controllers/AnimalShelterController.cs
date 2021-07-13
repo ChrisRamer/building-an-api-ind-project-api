de61e2b0119a8e805a25ec529d76f9de1ac32feb
Chris Ramer <chrisramer.97@gmail.com>
2021-07-13 11:28:30 -0700

    Add additional routes

--- AnimalShelterApi/Controllers/AnimalShelterController.cs
+++ AnimalShelterApi/Controllers/AnimalShelterController.cs
@@ -51,5 +51,66 @@ namespace AnimalShelterApi.Controllers
 
 			return query.ToList();
 		}
+
+		// GET animals/{id}
+		[HttpGet("{id}")]
+		public ActionResult<Animal> Get(int id)
+		{
+			return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+		}
+	}
+
+	[ApiVersion("2.0")]
+	[Route("v{version:apiVersion}/animals")]
+	[ApiController]
+	public class AnimalShelterV2Controller : ControllerBase
+	{
+		private AnimalShelterApiContext _db;
+
+		public AnimalShelterV2Controller(AnimalShelterApiContext db)
+		{
+			_db = db;
+		}
+
+		// GET /animals
+		[HttpGet]
+		public ActionResult<IEnumerable<Animal>> Get(int id, string name, string species, string gender, int age)
+		{
+			var query = _db.Animals.AsQueryable();
+
+			if (id > 0)
+			{
+				query = query.Where(entry => entry.AnimalId == id);
+			}
+
+			if (name != null)
+			{
+				query = query.Where(entry => entry.Name == name);
+			}
+
+			if (species != null)
+			{
+				query = query.Where(entry => entry.Species == species);
+			}
+
+			if (gender != null)
+			{
+				query = query.Where(entry => entry.Gender == gender);
+			}
+
+			if (age > 0)
+			{
+				query = query.Where(entry => entry.Age == age);
+			}
+
+			return query.ToList();
+		}
+
+		// GET animals/{id}
+		[HttpGet("{id}")]
+		public ActionResult<Animal> Get(int id)
+		{
+			return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+		}
 	}
 }
